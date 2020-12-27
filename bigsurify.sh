ff6757c396215ea6d9159cb9e2a3e01ca1c865cb
Luis Puerto <luis@luispuerto.net>
2020-12-27 01:50:38 +0100

    First commit
    App works more or less with all the apps I want it to work

---
+++ bigsurify.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env sh
+
+function bigsurify () {
+
+  sudo fileicon set /Applications/1Blocker.app /Users/lpuerto/Software/Big\ Sur\ Icons/1Blocker.icns
+  # sudo fileicon set /Applications/1Password\ 7.app /Users/lpuerto/Software/Big\ Sur\ Icons/1Password.icns
+  sudo fileicon set /Applications/A\ Better\ Finder\ Attributes\ 7.app /Users/lpuerto/Software/Big\ Sur\ Icons/A\ Better\ Finder\ Attributes.icns
+  sudo fileicon set /Applications/A\ Better\ Finder\ Rename\ 11.app /Users/lpuerto/Software/Big\ Sur\ Icons/A\ Better\ Finder\ Renamer.icns
+  sudo fileicon set /Applications/Adobe\ Acrobat\ DC /Users/lpuerto/Software/Big\ Sur\ Icons/Adobe_Acrobat.icns
+  sudo fileicon set /Applications/Adobe\ Acrobat\ DC/Acrobat\ Distiller.app /Users/lpuerto/Software/Big\ Sur\ Icons/Adobe_Distiller.icns
+  sudo fileicon set /Applications/Adobe\ Acrobat\ DC/Adobe\ Acrobat.app /Users/lpuerto/Software/Big\ Sur\ Icons/Adobe_Acrobat_Pro.icns
+  sudo fileicon set /Applications/Airfoil\ Satellite.app /Users/lpuerto/Software/Big\ Sur\ Icons/AirFoilSatellite.icns
+  sudo fileicon set /Applications/Airfoil.app /Users/lpuerto/Software/Big\ Sur\ Icons/Airfoil.icns
+  sudo fileicon set /Applications/Angry\ IP\ Scanner.app /Users/lpuerto/Software/Big\ Sur\ Icons/Angry\ IP\ scaner.icns
+  sudo fileicon set /Applications/Aperture.app /Users/lpuerto/Software/Big\ Sur\ Icons/Aperture.icns
+  sudo fileicon set /Applications/AppCleaner.app /Users/lpuerto/Software/Big\ Sur\ Icons/AppCleaner_Alt.icns
+  sudo fileicon set /Applications/AutoFirma.app /Users/lpuerto/Software/Big\ Sur\ Icons/Autofirma.icns
+  sudo fileicon set /Applications/balenaEtcher.app /Users/lpuerto/Software/Big\ Sur\ Icons/balenaEtcher.icns
+  sudo fileicon set /Applications/Be\ Focused\ Pro.app /Users/lpuerto/Software/Big\ Sur\ Icons/Be_Focused.icns
+  sudo fileicon set /Applications/BitdefenderVirusScanner.app /Users/lpuerto/Software/Big\ Sur\ Icons/BitDefender.icns
+  sudo fileicon set /Applications/BookMacster.app /Users/lpuerto/Software/Big\ Sur\ Icons/BookMacster.icns
+  sudo fileicon set /Applications/calibre.app /Users/lpuerto/Software/Big\ Sur\ Icons/Calibre.icns
+  sudo fileicon set /Applications/Chromium.app /Users/lpuerto/Software/Big\ Sur\ Icons/Chromium.icns
+  sudo fileicon set /Applications/Chrono\ Plus.app /Users/lpuerto/Software/Big\ Sur\ Icons/Chrono\ Plus.icns
+  sudo fileicon set /Applications/CloudCompare /Users/lpuerto/Software/Big\ Sur\ Icons/Cloud\ Compare.icns
+  sudo fileicon set /Applications/CloudCompare/CloudCompare.app /Users/lpuerto/Software/Big\ Sur\ Icons/Cloud\ Compare.icns
+  sudo fileicon set /Applications/CloudCompare/ccViewer.app /Users/lpuerto/Software/Big\ Sur\ Icons/Cloud\ Compare.icns
+  sudo fileicon set /Applications/Contacts\ Sync\ for\ Google\ Gmail.app /Users/lpuerto/Software/Big\ Sur\ Icons/Contacts\ Sync\ for\ Google\ Gmail.icns
+  sudo fileicon set /Applications/Cyberduck.app /Users/lpuerto/Software/Big\ Sur\ Icons/Cyberduck.icns
+  sudo fileicon set /Applications/DeepL.app /Users/lpuerto/Software/Big\ Sur\ Icons/DeepL.icns
+  sudo fileicon set /Applications/DEVONthink\ 3.app /Users/lpuerto/Software/Big\ Sur\ Icons/DevonThink.icns
+  sudo fileicon set /Applications/Discord.app /Users/lpuerto/Software/Big\ Sur\ Icons/Discord.icns
+  # sudo fileicon set /Applications/Dropbox.app
+  # sudo fileicon set /Applications/Epson\ Software
+  # sudo fileicon set /Applications/Figma.app
+  sudo fileicon set /Applications/Firefox.app /Users/lpuerto/Software/Big\ Sur\ Icons/Firefox.icns
+  # sudo fileicon set /Applications/Fork.app
+  sudo fileicon set /Applications/Get\ Lyrical.app /Users/lpuerto/Software/Big\ Sur\ Icons/Get\ Lyrical.icns
+  sudo fileicon set /Applications/Gifox.app /Users/lpuerto/Software/Big\ Sur\ Icons/Gifox.icns
+  sudo fileicon set /Applications/GitHub\ Desktop.app /Users/lpuerto/Software/Big\ Sur\ Icons/GitHub.icns
+  sudo fileicon set /Applications/GoodNotes.app /Users/lpuerto/Software/Big\ Sur\ Icons/GoodNotes.icns
+  sudo fileicon set /Applications/GPG\ Keychain.app /Users/lpuerto/Software/Big\ Sur\ Icons/GPG\ Keychain.icns
+  sudo fileicon set /Applications/Image2Icon.app /Users/lpuerto/Software/Big\ Sur\ Icons/Image2Icon.icns
+  sudo fileicon set /Applications/ImageOptim.app /Users/lpuerto/Software/Big\ Sur\ Icons/ImageOptim.icns
+  # sudo fileicon set /Applications/iTerm.app /Users/lpuerto/Software/Big\ Sur\ Icons/iTerm.icns
+  sudo fileicon set /Applications/Jitsi\ Meet.app /Users/lpuerto/Software/Big\ Sur\ Icons/Jitsi.icns
+  sudo fileicon set /Applications/Julia-*.app /Users/lpuerto/Software/Big\ Sur\ Icons/Julia.icns
+  sudo fileicon set /Applications/JustFocus.app /Users/lpuerto/Software/Big\ Sur\ Icons/JustFocus.icns
+  sudo fileicon set /Applications/Kaleidoscope.app /Users/lpuerto/Software/Big\ Sur\ Icons/Kaleidoscope.icns
+  sudo fileicon set /Applications/Keybase.app /Users/lpuerto/Software/Big\ Sur\ Icons/Keybase.icns
+  sudo fileicon set /Applications/KeyboardCleanTool.app /Users/lpuerto/Software/Big\ Sur\ Icons/KeyboardCleanTool.icns
+  # sudo fileicon set /Applications/Keynote.app 
+  sudo fileicon set /Applications/Kindle.app /Users/lpuerto/Software/Big\ Sur\ Icons/Kindle.icns
+  sudo fileicon set /Applications/LibraryAccess.app /Users/lpuerto/Software/Big\ Sur\ Icons/Library\ Access.icns
+  sudo fileicon set /Applications/Luminar\ 4.app /Users/lpuerto/Software/Big\ Sur\ Icons/Luminar.icns
+  sudo fileicon set /Applications/Macs\ Fan\ Control.app /Users/lpuerto/Software/Big\ Sur\ Icons/smcFanControl.icns
+  sudo fileicon set /Applications/Magnet.app /Users/lpuerto/Software/Big\ Sur\ Icons/Magnet.icns
+  sudo fileicon set /Applications/Marked\ 2.app /Users/lpuerto/Software/Big\ Sur\ Icons/Marked2\ alt.icns
+  sudo fileicon set /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app /Users/lpuerto/Software/Big\ Sur\ Icons/MSupdater.icns
+  sudo fileicon set /Applications/Microsoft\ Excel.app /Users/lpuerto/Software/Big\ Sur\ Icons/Microsoft_Excel.icns
+  sudo fileicon set /Applications/Microsoft\ OneNote.app /Users/lpuerto/Software/Big\ Sur\ Icons/Microsoft_OneNote.icns
+  sudo fileicon set /Applications/Microsoft\ Outlook.app /Users/lpuerto/Software/Big\ Sur\ Icons/Microsoft_Outlook.icns
+  sudo fileicon set /Applications/Microsoft\ PowerPoint.app /Users/lpuerto/Software/Big\ Sur\ Icons/Microsoft_Powerpoint.icns
+  sudo fileicon set /Applications/Microsoft\ Teams.app /Users/lpuerto/Software/Big\ Sur\ Icons/Microosft\ Teams.icns
+  sudo fileicon set /Applications/Microsoft\ Word.app /Users/lpuerto/Software/Big\ Sur\ Icons/Microsoft_Word.icns
+  sudo fileicon set /Applications/Movist.app /Users/lpuerto/Software/Big\ Sur\ Icons/Movist.icns
+  sudo fileicon set /Applications/MusicBrainz\ Picard.app /Users/lpuerto/Software/Big\ Sur\ Icons/MusicBrainz\ Picard.icns
+  sudo fileicon set /Applications/NordVPN\ IKE.app /Users/lpuerto/Software/Big\ Sur\ Icons/NordVPN_IKE.icns
+  sudo fileicon set /Applications/Notion.app /Users/lpuerto/Software/Big\ Sur\ Icons/Notion.icns
+  # sudo fileicon set /Applications/Numbers.app
+  sudo fileicon set /Applications/OCRApp.app /Users/lpuerto/Software/Big\ Sur\ Icons/OCRApp.icns
+  # sudo fileicon set /Applications/OmniGraffle.app
+  sudo fileicon set /Applications/OmniPlan.app /Users/lpuerto/Software/Big\ Sur\ Icons/OmmiPlan.icns
+  sudo fileicon set /Applications/OneDrive.app /Users/lpuerto/Software/Big\ Sur\ Icons/Microosft\ OneDrive.icns
+  # sudo fileicon set /Applications/OnyX.app
+  sudo fileicon set /Applications/OpenRA\ -\ Dune\ 2000.app /Users/lpuerto/Software/Big\ Sur\ Icons/OpenRA\ Dune.icns
+  sudo fileicon set /Applications/OpenRA\ -\ Red\ Alert.app /Users/lpuerto/Software/Big\ Sur\ Icons/OpenRA\ Red\ Alert.icns
+  sudo fileicon set /Applications/OpenRA\ -\ Tiberian\ Dawn.app /Users/lpuerto/Software/Big\ Sur\ Icons/OpenRA\ Tiberian\ Sun.icns
+  sudo fileicon set /Applications/Paparazzi!.app /Users/lpuerto/Software/Big\ Sur\ Icons/Paparazzi.icns
+  sudo fileicon set /Applications/Papers.app /Users/lpuerto/Software/Big\ Sur\ Icons/Papers\ 4.icns
+  # sudo fileicon set /Applications/Parcel.app
+  sudo fileicon set /Applications/PDF\ Toolkit+.app /Users/lpuerto/Software/Big\ Sur\ Icons/PDFToolKit.icns
+  # sudo fileicon set /Applications/Pixelmator\ Pro.app
+  # sudo fileicon set /Applications/Playgrounds.app
+  sudo fileicon set /Applications/QGIS.app /Users/lpuerto/Software/Big\ Sur\ Icons/QGIS.icns
+  # sudo fileicon set /Applications/Rectangle.app
+  # sudo fileicon set /Applications/Reeder.app
+  sudo fileicon set /Applications/RStudio.app /Users/lpuerto/Software/Big\ Sur\ Icons/RStudio\ Alt.icns
+  sudo fileicon set /Applications/Signal.app /Users/lpuerto/Software/Big\ Sur\ Icons/Signal.icns
+  sudo fileicon set /Applications/Skype.app /Users/lpuerto/Software/Big\ Sur\ Icons/Skype.icns
+  sudo fileicon set /Applications/Slack.app /Users/lpuerto/Software/Big\ Sur\ Icons/Slack.icns
+  # sudo fileicon set /Applications/Streaks.app
+  sudo fileicon set /Applications/Sublime\ Merge.app /Users/lpuerto/Software/Big\ Sur\ Icons/Sublime_Merge.icns
+  sudo fileicon set /Applications/Sublime\ Text.app /Users/lpuerto/Software/Big\ Sur\ Icons/Sublime_Text.icns
+  sudo fileicon set /Applications/Sync\ Folders\ Pro.app /Users/lpuerto/Software/Big\ Sur\ Icons/Sync\ Folders\ Pro.icns
+  sudo fileicon set /Applications/TeamViewer.app /Users/lpuerto/Software/Big\ Sur\ Icons/Teamviewer.icns
+  # sudo fileicon set /Applications/Telegram.app
+  sudo fileicon set /Applications/TeX /Users/lpuerto/Software/Big\ Sur\ Icons/TexShop.icns
+  sudo fileicon set /Applications/TeX/TeXShop.app /Users/lpuerto/Software/Big\ Sur\ Icons/TexShop.icns
+  sudo fileicon set /Applications/TeX/LaTeXiT.app /Users/lpuerto/Software/Big\ Sur\ Icons/LaTeXiT.icns
+  sudo fileicon set /Applications/Texpad.app /Users/lpuerto/Software/Big\ Sur\ Icons/Texpad.icns
+  sudo fileicon set /Applications/Textual\ 7.app /Users/lpuerto/Software/Big\ Sur\ Icons/Textual.icns
+  sudo fileicon set /Applications/The\ Unarchiver.app /Users/lpuerto/Software/Big\ Sur\ Icons/The_Unarchiver.icns
+  # sudo fileicon set /Applications/Things3.app
+  # sudo fileicon set /Applications/TomTom\ MyDrive\ Connect.app /Users/lpuerto/Software/Big\ Sur\ Icons/TomTom.icns
+  sudo fileicon set /Applications/TranslateMe.app /Users/lpuerto/Software/Big\ Sur\ Icons/TranslateMe.icns
+  sudo fileicon set /Applications/Transmission.app /Users/lpuerto/Software/Big\ Sur\ Icons/Transmission.icns
+  # sudo fileicon set /Applications/Transmit.app /Users/lpuerto/Software/Big\ Sur\ Icons/Transmit.icns
+  sudo fileicon set /Applications/Trash\ It!.app /Users/lpuerto/Software/Big\ Sur\ Icons/TrashIt.icns
+  sudo fileicon set /Applications/Tunnelblick.app /Users/lpuerto/Software/Big\ Sur\ Icons/Tunnelblick.icns
+  sudo fileicon set /Applications/Tuxera\ Disk\ Manager.app /Users/lpuerto/Software/Big\ Sur\ Icons/Tuxera\ Disk\ Manager.icns
+  # sudo fileicon set /Applications/Twitter.app
+  sudo fileicon set /Applications/Typora.app /Users/lpuerto/Software/Big\ Sur\ Icons/Typora.icns
+  sudo fileicon set /Applications/Utilities/XQuartz.app /Users/lpuerto/Software/Big\ Sur\ Icons/XQuartz_Alt.icns
+  sudo fileicon set /Applications/VNC\ Viewer.app /Users/lpuerto/Software/Big\ Sur\ Icons/VNC_Viewer.icns
+  # sudo fileicon set /Applications/Wipr.app
+  sudo fileicon set /Applications/WireGuard.app /Users/lpuerto/Software/Big\ Sur\ Icons/WireGuard.icns
+  # sudo fileicon set /Applications/Xcode.app
+  sudo fileicon set /Applications/XLD.app /Users/lpuerto/Software/Big\ Sur\ Icons/XLD.icns
+  sudo fileicon set /Applications/zoom.us.app /Users/lpuerto/Software/Big\ Sur\ Icons/Zoom.icns
+  
+  killall Dock
+  killall Finder
+}
