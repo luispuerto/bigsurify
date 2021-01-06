f14acac2cc22acf3802177b5fc82d2b513fba88c
Luis Puerto <luis@luispuerto.net>
2021-01-06 10:25:20 +0100

    Add PhotoSweeper app
    I use the icon from Photos Finder because the one from iOS is really
    ugly.

--- bigsurify.sh
+++ bigsurify.sh
@@ -81,6 +81,7 @@ function bigsurify () {
   sudo fileicon set /Applications/Papers.app /Users/lpuerto/Software/Big\ Sur\ Icons/Papers\ 4\ alt.icns
   # sudo fileicon set /Applications/Parcel.app
   sudo fileicon set /Applications/PDF\ Toolkit+.app /Users/lpuerto/Software/Big\ Sur\ Icons/PDFToolKit.icns
+  sudo fileicon set /Applications/PhotoSweeper.app /Users/lpuerto/Software/Big\ Sur\ Icons/Photos\ Finder.icns
   # sudo fileicon set /Applications/Pixelmator\ Pro.app
   # sudo fileicon set /Applications/Playgrounds.app
   sudo fileicon set /Applications/QGIS.app /Users/lpuerto/Software/Big\ Sur\ Icons/QGIS.icns
