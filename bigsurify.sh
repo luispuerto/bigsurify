f4d07eb682901be233f09b27a8228a7328c1a35e
Luis Puerto <luis@luispuerto.net>
2022-03-02 16:26:01 +0100

    Reorganize actions alphabetically and commenting
    Also adding some missing icons from the Quantis computer

--- bigsurify.sh
+++ bigsurify.sh
@@ -2,22 +2,22 @@
 
 function bigsurify () {
 
-  #Close Mail and Things before you start. 
+  # Close Mail and Things before you start. 
   pkill -x Mail Things3 
+
+  # Icon actions
   sudo fileicon set /Applications/A\ Better\ Finder\ Attributes\ 7.app ~/Software/Big\ Sur\ Icons/A\ Better\ Finder\ Attributes.icns
   sudo fileicon set /Applications/A\ Better\ Finder\ Rename\ 11.app ~/Software/Big\ Sur\ Icons/A\ Better\ Finder\ Renamer.icns
-  sudo fileicon set /Applications/Airfoil\ Satellite.app ~/Software/Big\ Sur\ Icons/AirFoilSatellite.icns
   sudo fileicon set /Applications/Airfoil.app ~/Software/Big\ Sur\ Icons/Airfoil.icns
+  sudo fileicon set /Applications/Airfoil\ Satellite.app ~/Software/Big\ Sur\ Icons/AirFoilSatellite.icns
   sudo fileicon set /Applications/Amphetamine\ Enhancer.app ~/Software/Big\ Sur\ Icons/Amphetamine\ Enhancer.icns
   sudo fileicon set /Applications/Angry\ IP\ Scanner.app ~/Software/Big\ Sur\ Icons/Angry\ IP\ scaner.icns
   sudo fileicon set /Applications/Aperture.app ~/Software/Big\ Sur\ Icons/Aperture.icns
   sudo fileicon set /Applications/AppCleaner.app ~/Software/Big\ Sur\ Icons/AppCleaner_Alt.icns
   sudo fileicon set /Applications/AutoFirma.app ~/Software/Big\ Sur\ Icons/Autofirma.icns
-  sudo fileicon set /Applications/balenaEtcher.app ~/Software/Big\ Sur\ Icons/balenaEtcher.icns
+  sudo fileicon set /Applications/Backup\ and\ Sync.app/ ~/Software/Big\ Sur\ Icons/Google\ Backup\ and\ Sync.icns  
   sudo fileicon set /Applications/Be\ Focused\ Pro.app ~/Software/Big\ Sur\ Icons/Be_Focused.icns
-  sudo iconsur set -l /Applications/Bluesnooze.app
   sudo fileicon set /Applications/BookMacster.app ~/Software/Big\ Sur\ Icons/BookMacster.icns
-  sudo fileicon set /Applications/calibre.app ~/Software/Big\ Sur\ Icons/Calibre.icns
   sudo fileicon set /Applications/Chromium.app ~/Software/Big\ Sur\ Icons/Chromium.icns
   sudo fileicon set /Applications/Chrono\ Plus.app ~/Software/Big\ Sur\ Icons/Chrono\ Plus.icns
   sudo fileicon set /Applications/CloudCompare ~/Software/Big\ Sur\ Icons/Cloud\ Compare.icns
@@ -31,10 +31,16 @@ function bigsurify () {
   sudo fileicon set /Applications/Dual\ Controller.app ~/Software/Big\ Sur\ Icons/Dual\ Controller.icns
   sudo fileicon set /Applications/Epic\ Games\ Launcher.app/ ~/Software/Big\ Sur\ Icons/Epic\ Games.icns
   sudo fileicon set /Applications/Firefox.app ~/Software/Big\ Sur\ Icons/Firefox.icns
+  sudo fileicon set /Applications/GPG\ Keychain.app ~/Software/Big\ Sur\ Icons/GPG\ Keychain.icns
   sudo fileicon set /Applications/Get\ Lyrical.app ~/Software/Big\ Sur\ Icons/Get\ Lyrical.icns
   sudo fileicon set /Applications/Gifox.app ~/Software/Big\ Sur\ Icons/Gifox.icns
   sudo fileicon set /Applications/GitHub\ Desktop.app ~/Software/Big\ Sur\ Icons/GitHub.icns
-  sudo fileicon set /Applications/GPG\ Keychain.app ~/Software/Big\ Sur\ Icons/GPG\ Keychain.icns
+  sudo fileicon set /Applications/GoToMeeting\ \(18705\).app ~/Software/Big\ Sur\ Icons/GoToMeeting.icns
+  sudo fileicon set /Applications/Google\ Chrome.app/ ~/Software/Big\ Sur\ Icons/Google\ Chrome.icns
+  sudo fileicon set /Applications/Google\ Docs.app/ ~/Software/Big\ Sur\ Icons/Google\ Docs.icns
+  sudo fileicon set /Applications/Google\ Drive.app/ ~/Software/Big\ Sur\ Icons/Google\ Drive.icns
+  sudo fileicon set /Applications/Google\ Sheets.app/ ~/Software/Big\ Sur\ Icons/Google\ Sheets.icns
+  sudo fileicon set /Applications/Google\ Slides.app/ ~/Software/Big\ Sur\ Icons/Google\ Slides.icns
   sudo fileicon set /Applications/ImageOptim.app ~/Software/Big\ Sur\ Icons/ImageOptim.icns
   sudo fileicon set /Applications/Jitsi\ Meet.app ~/Software/Big\ Sur\ Icons/Jitsi.icns
   sudo fileicon set /Applications/Julia-*.app ~/Software/Big\ Sur\ Icons/Julia.icns
@@ -43,47 +49,45 @@ function bigsurify () {
   sudo fileicon set /Applications/Keybase.app ~/Software/Big\ Sur\ Icons/Keybase.icns
   sudo fileicon set /Applications/KeyboardCleanTool.app ~/Software/Big\ Sur\ Icons/KeyboardCleanTool.icns
   sudo fileicon set /Applications/LG\ Calibration\ Studio.app/ ~/Software/Big\ Sur\ Icons/LG\ Calibration\ Studio.icns
+  sudo fileicon set /Applications/Launchpad\ Manager.app ~/Software/Big\ Sur\ Icons/Launchpad\ Manager.icns
   sudo fileicon set /Applications/Luminar\ 4.app ~/Software/Big\ Sur\ Icons/Luminar.icns
   sudo fileicon set /Applications/Macs\ Fan\ Control.app ~/Software/Big\ Sur\ Icons/smcFanControl.icns
   sudo fileicon set /Applications/Magnet.app ~/Software/Big\ Sur\ Icons/Magnet.icns
-  sudo fileicon set /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app ~/Software/Big\ Sur\ Icons/Microsoft\ Updater\ alt.icns
+  sudo fileicon set /Applications/Managed\ Software\ Center.app ~/Software/Big\ Sur\ Icons/Managed\ Software\ Centre.icns
   sudo fileicon set /Applications/Microsoft\ Excel.app ~/Software/Big\ Sur\ Icons/Microsoft_Excel.icns
   sudo fileicon set /Applications/Microsoft\ OneNote.app ~/Software/Big\ Sur\ Icons/Microsoft_OneNote.icns
   sudo fileicon set /Applications/Microsoft\ Outlook.app ~/Software/Big\ Sur\ Icons/Microsoft_Outlook.icns
   sudo fileicon set /Applications/Microsoft\ PowerPoint.app ~/Software/Big\ Sur\ Icons/Microsoft_Powerpoint.icns
   sudo fileicon set /Applications/Microsoft\ Teams.app ~/Software/Big\ Sur\ Icons/Microosft\ Teams.icns
   sudo fileicon set /Applications/Microsoft\ Word.app ~/Software/Big\ Sur\ Icons/Microsoft_Word.icns
-  sudo iconsur set -l -s 1.2 /Applications/MysteriumVPN.app
   sudo fileicon set /Applications/Movist.app ~/Software/Big\ Sur\ Icons/Movist.icns
   sudo fileicon set /Applications/MusicBrainz\ Picard.app ~/Software/Big\ Sur\ Icons/MusicBrainz\ Picard.icns
   sudo fileicon set /Applications/NordVPN\ IKE.app ~/Software/Big\ Sur\ Icons/NordVPN_IKE.icns
   sudo fileicon set /Applications/Notion.app ~/Software/Big\ Sur\ Icons/Notion.icns
   sudo fileicon set /Applications/Obsidian.app ~/Software/Big\ Sur\ Icons/Obsidian.icns
   sudo fileicon set /Applications/OneDrive.app ~/Software/Big\ Sur\ Icons/Microosft\ OneDrive.icns
-  sudo iconsur set -l /Applications/OnScreen\ Control.app
   sudo fileicon set /Applications/OpenRA\ -\ Dune\ 2000.app ~/Software/Big\ Sur\ Icons/OpenRA\ Dune.icns
   sudo fileicon set /Applications/OpenRA\ -\ Red\ Alert.app ~/Software/Big\ Sur\ Icons/OpenRA\ Red\ Alert.icns
   sudo fileicon set /Applications/OpenRA\ -\ Tiberian\ Dawn.app ~/Software/Big\ Sur\ Icons/OpenRA\ Tiberian\ Sun.icns
+  sudo fileicon set /Applications/PDF\ Toolkit+.app ~/Software/Big\ Sur\ Icons/PDFToolKit.icns
   sudo fileicon set /Applications/Paparazzi!.app ~/Software/Big\ Sur\ Icons/Paparazzi.icns
   sudo fileicon set /Applications/Papers.app ~/Software/Big\ Sur\ Icons/Papers\ 4\ alt.icns
-  sudo fileicon set /Applications/PDF\ Toolkit+.app ~/Software/Big\ Sur\ Icons/PDFToolKit.icns
-  sudo iconsur set -l /Applications/PhotoMill.app
-  sudo iconsur set -l /Applications/PhotoSweeper.app
   sudo fileicon set /Applications/QGIS.app ~/Software/Big\ Sur\ Icons/QGIS\ Alt\ 2.icns
   sudo fileicon set /Applications/R.app ~/Software/Big\ Sur\ Icons/R.icns
-  sudo fileicon set /Applications/ReadCube\ Papers\ Extension.app ~/Software/Big\ Sur\ Icons/Papers\ 4\ alt.icns
   sudo fileicon set /Applications/RStudio.app ~/Software/Big\ Sur\ Icons/RStudio\ Alt.icns
+  sudo fileicon set /Applications/ReadCube\ Papers\ Extension.app ~/Software/Big\ Sur\ Icons/Papers\ 4\ alt.icns
   sudo fileicon set /Applications/Skype.app ~/Software/Big\ Sur\ Icons/Skype.icns
   sudo fileicon set /Applications/Slack.app ~/Software/Big\ Sur\ Icons/Slack.icns
+  sudo fileicon set /Applications/Sophos ~/Software/Big\ Sur\ Icons/Sophos\ Home.icns 
   sudo fileicon set /Applications/Sublime\ Merge.app ~/Software/Big\ Sur\ Icons/Sublime_Merge.icns
   sudo fileicon set /Applications/Sublime\ Text.app ~/Software/Big\ Sur\ Icons/Sublime_Text.icns
   sudo fileicon set /Applications/Sync\ Folders\ Pro.app ~/Software/Big\ Sur\ Icons/Sync\ Folders\ Pro.icns
   sudo fileicon set /Applications/Tampermonkey.app/ ~/Software/Big\ Sur\ Icons/Tampermonkey.icns
-  sudo fileicon set /Applications/TeamViewer.app ~/Software/Big\ Sur\ Icons/Teamviewer.icns
   sudo fileicon set /Applications/TeX ~/Software/Big\ Sur\ Icons/TexShop.icns
-  sudo fileicon set /Applications/TeX/TeXShop.app ~/Software/Big\ Sur\ Icons/TexShopApp.icns
   sudo fileicon set /Applications/TeX/LaTeXiT.app ~/Software/Big\ Sur\ Icons/LaTeXiT.icns
+  sudo fileicon set /Applications/TeX/TeXShop.app ~/Software/Big\ Sur\ Icons/TexShopApp.icns
   sudo fileicon set /Applications/TeX/TeX\ Live\ Utility.app ~/Software/Big\ Sur\ Icons/TeX\ Live\ Utility.icns
+  sudo fileicon set /Applications/TeamViewer.app ~/Software/Big\ Sur\ Icons/Teamviewer.icns
   sudo fileicon set /Applications/Texpad.app ~/Software/Big\ Sur\ Icons/Texpad.icns
   sudo fileicon set /Applications/Textual\ 7.app ~/Software/Big\ Sur\ Icons/Textual.icns
   sudo fileicon set /Applications/The\ Unarchiver.app ~/Software/Big\ Sur\ Icons/The_Unarchiver.icns
@@ -96,9 +100,21 @@ function bigsurify () {
   sudo fileicon set /Applications/VNC\ Viewer.app ~/Software/Big\ Sur\ Icons/VNC_Viewer.icns
   sudo fileicon set /Applications/WireGuard.app ~/Software/Big\ Sur\ Icons/WireGuard.icns
   sudo fileicon set /Applications/XLD.app ~/Software/Big\ Sur\ Icons/XLD.icns
+  sudo fileicon set /Applications/balenaEtcher.app ~/Software/Big\ Sur\ Icons/balenaEtcher.icns
+  sudo fileicon set /Applications/calibre.app ~/Software/Big\ Sur\ Icons/Calibre.icns
   sudo fileicon set /Applications/zoom.us.app ~/Software/Big\ Sur\ Icons/Zoom.icns
-  
+  sudo fileicon set /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app ~/Software/Big\ Sur\ Icons/Microsoft\ Updater\ alt.icns
+  sudo iconsur set -l -s 1.2 /Applications/MysteriumVPN.app
+  sudo iconsur set -l /Applications/Bluesnooze.app
+  sudo iconsur set -l /Applications/OnScreen\ Control.app
+  sudo iconsur set -l /Applications/PhotoMill.app
+  sudo iconsur set -l /Applications/PhotoSweeper.app
+  sudo iconsur set -l /Applications/Self-Service.app
+
+  # Killing Dock and Finder to the new icons load properly
   killall Dock Finder
+  
+  # Reopening Mail and Things
   open -a Mail
   open -a Things3
 }
