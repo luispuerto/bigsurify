7d92d114ab9242dad43e5bef1b691661e8c00a3d
Luis Puerto <luis@luispuerto.net>
2023-02-13 22:07:21 +0100

    Add Anaconda Navigator

--- bigsurify.sh
+++ bigsurify.sh
@@ -123,6 +123,7 @@ function bigsurify () {
   sudo fileicon set /Applications/zoom.us.app ~/Documents/Library/Big\ Sur\ Icons/Zoom.icns
   sudo fileicon set /Applications/Zotero.app ~/Documents/Library/Big\ Sur\ Icons/Zotero.icns
   sudo fileicon set /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app ~/Documents/Library/Big\ Sur\ Icons/Microsoft\ Updater\ alt.icns
+  sudo fileicon set /opt/homebrew/anaconda3/Anaconda-Navigator.app /Users/luispuerto/Documents/Library/Big\ Sur\ Icons/Anaconda.icns
   sudo fileicon set /opt/homebrew/Caskroom/miniforge/base/Anaconda-Navigator.app /Users/luispuerto/Documents/Library/Big\ Sur\ Icons/Anaconda.icns
   sudo fileicon set /Users/luispuerto/Applications/Chrome\ Apps.localized/Google\ Meet.app ~/Documents/Library/Big\ Sur\ Icons/Google\ Meet.icns
   sudo fileicon set /Users/luispuerto/Library/CloudStorage/GoogleDrive-luis.puerto@quantis-intl.com /Users/luispuerto/Documents/Library/Big\ Sur\ Icons/Folders/Google\ Drive\ Folder.icns
